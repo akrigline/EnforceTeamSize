4830043befcd2ef19a3071356f063044306822b6
Andrew Krigline <andrew@akrigline.com>
2014-10-26 18:01:41 -0400

    Works!

--- output/lua/shine/extensions/enforceteamsize.lua
+++ output/lua/shine/extensions/enforceteamsize.lua
@@ -1,28 +1,30 @@
 local Plugin = {}
 
-Plugin.HasConfig = true --Does this plugin have a config file?
-Plugin.ConfigName = "enforceteamsize.json" --What's the name of the file?
-Plugin.DefaultConfig = { --What's the default config setup?
-    MaxPlayers = 16
+Plugin.HasConfig = true
+Plugin.ConfigName = "enforceteamsize.json"
+Plugin.DefaultConfig = {
+    MaxPlayers = 16,
+	TooManyMessage = "Please Spectate until the round ends."
 }
-Plugin.CheckConfig = true --Should we check for missing/unused entries when loading?
-Plugin.CheckConfigTypes = true --Should we check the types of values in the config to make sure they match our default's types?
+Plugin.CheckConfig = true
+Plugin.CheckConfigTypes = true
 
-Plugin.DefaultState = false --Should the plugin be enabled when it is first added to the config?
-
-function Plugin:JoinTeam(gamerules, player, newteam, force)
-	local cancel = false
-	if Shine.GetAllPlayers() > MaxPlayers then 
-		cancel
-	end
-	if cancel then
-		return false
-	end
-end
+Plugin.DefaultState = false
 
 function Plugin:Initialise()
     self.Enabled = true
     return true
+end
+
+function Plugin:JoinTeam(gamerules, player, newteam, force, ShineForce)
+	if ShineForce or newteam == kSpectatorIndex or newteam == kTeamReadyRoom then return end
+	return self:Check()
+end
+
+function Plugin:Check()	
+	if #Shine.GetAllPlayers() > self.Config.MaxPlayers then
+		return false
+	end
 end
 
 function Plugin:Cleanup()
@@ -30,4 +32,4 @@ function Plugin:Cleanup()
     self.BaseClass.Cleanup( self )
 end
 
-Shine:RegisterExtension("enforceteamsizes", Plugin )
+Shine:RegisterExtension("enforceteamsize", Plugin )
